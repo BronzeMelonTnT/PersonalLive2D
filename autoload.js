e14a9363db0cee0bcb96a7821e600f9bee6ac53d
BronzeMelonTnT <2504371473@qq.com>
2025-02-19 18:04:05 +0800

    更改path

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/BronzeMelonTnT/PersonalLive2D@latest/";
+const live2d_path = "https://npm.elemecdn.com/akilar-live2dapi@latest/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
